358dcb23768e6abd6dd447130510930c47722d5b
faaf <ogabrielclemos@gmail.com>
2025-05-21 14:01:53 -0300

    Initial commit

---
+++ src/main/java/net/faaf/testno1/TestModClient.java
@@ -0,0 +1,10 @@
+package net.faaf.testno1;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/faaf/testno1/TestNo1.java
@@ -0,0 +1,17 @@
+package net.faaf.testno1;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestNo1 implements ModInitializer {
+	public static final String MOD_ID = "testno1";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
